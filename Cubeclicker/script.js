6ee8eb3722e6cc23252eaaf3a9f5fd3999ff6592
frkatona <apk5101@psu.edu>
2023-03-26 15:57:31 -0400

    updated homepage

---
+++ Cubeclicker/script.js
@@ -0,0 +1,41 @@
+const box = document.getElementById("box");
+const counter = document.getElementById("counter");
+
+let clickCount = 0;
+
+// creates list of audio files from folder
+const clickSounds = ['Audio/note1.mp3', 'Audio/note2.mp3', 'Audio/note3.mp3', 'Audio/note4.mp3', 'Audio/note5.mp3']
+
+function getRandomPosition() {
+    const windowHeight = window.innerHeight - box.offsetHeight;
+    const windowWidth = window.innerWidth - box.offsetWidth;
+    const randomY = Math.floor(Math.random() * windowHeight);
+    const randomX = Math.floor(Math.random() * windowWidth);
+
+    return { x: randomX, y: randomY };
+}
+
+function moveBox() {
+    // box movement
+	const position = getRandomPosition();
+    box.style.top = `${position.y}px`;
+    box.style.left = `${position.x}px`;
+
+	// audio select and play
+    const clickSound = new Audio(clickSounds[Math.floor(Math.random() * clickSounds.length)]);
+	clickSound.play();
+
+	// counter update
+    clickCount++;
+    counter.textContent = clickCount;
+}
+
+box.addEventListener("click", moveBox);
+
+// Dark Mode
+document.body.classList.add("dark-mode");
+const toggle = document.getElementById("dark-mode-toggle");
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+}
+toggle.addEventListener("change", toggleDarkMode);
--- script.js
+++ script.js
@@ -1,75 +1,22 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+document.addEventListener("DOMContentLoaded", function() {
+    const thumbnailContainers = document.querySelectorAll(".thumbnail-container");
 
-let cubeX = 50;
-let cubeY = 50;
-let clickCount = 0;
-let isMuted = false;
+    thumbnailContainers.forEach(function(thumbnailContainer) {
+        thumbnailContainer.addEventListener("mouseenter", function() {
+            this.style.transform = "scale(1.05)";
+            this.style.transition = "transform 0.3s";
+        });
 
-// Create array of sound file names
-const soundFiles = ['note1.mp3', 'note2.mp3', 'note3.mp3', 'note4.mp3', 'note5.mp3', 'note6.mp3', 'note7.mp3'];
+        thumbnailContainer.addEventListener("mouseleave", function() {
+            this.style.transform = "scale(1)";
+            this.style.transition = "transform 0.3s";
+        });
+    });
 
-// Create audio element for background music and load sound file
-const bgMusic = document.getElementById('bg-music');
-bgMusic.volume = 0.1; // Set music volume to 50%
-bgMusic.play(); // Start playing the music when the page loads
-
-function drawCube() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.fillStyle = 'red';
-	ctx.fillRect(cubeX, cubeY, 150, 150); // Make cube 3 times bigger
-
-	ctx.fillStyle = 'white';
-	ctx.font = '20px Arial';
-	ctx.fillText(`Click count: ${clickCount}`, 10, 30);
-}
-
-function resizeCanvas() {
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
-}
-
-resizeCanvas();
-drawCube();
-
-window.addEventListener('resize', function() {
-	resizeCanvas();
-	drawCube();
-});
-
-canvas.addEventListener('click', function(e) {
-	if (e.clientX >= cubeX && e.clientX <= cubeX + 150 && e.clientY >= cubeY && e.clientY <= cubeY + 150) {
-		cubeX = Math.floor(Math.random() * (canvas.width - 150));
-		cubeY = Math.floor(Math.random() * (canvas.height - 150));
-		clickCount++;
-
-		// Choose random sound file from array
-		const soundFile = soundFiles[Math.floor(Math.random() * soundFiles.length)];
-
-		// Create audio element and load selected sound file
-		const audio = new Audio(soundFile);
-
-		// Set volume for sound effect
-		audio.volume = 1.0; // Set volume to maximum
-
-		// Play sound effect
-		audio.currentTime = 0;
-		audio.play();
-
-		drawCube();
-	}
-});
-
-// Add event listener for mute button
-const muteBtn = document.getElementById('mute-btn');
-muteBtn.addEventListener('click', function() {
-	if (!isMuted) {
-		bgMusic.pause();
-		muteBtn.textContent = 'Unmute';
-		isMuted = true;
-	} else {
-		bgMusic.play();
-		muteBtn.textContent = 'Mute';
-		isMuted = false;
-	}
+    // Parallax scrolling effect
+    const banner = document.querySelector(".banner");
+    window.addEventListener("scroll", function() {
+        let scrollPos = window.pageYOffset;
+        banner.style.backgroundPositionY = -scrollPos * 0.5 + "px";
+    });
 });
