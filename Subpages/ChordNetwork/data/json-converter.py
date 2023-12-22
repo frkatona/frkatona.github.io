4a2aecdf88e8915edbd7ec4ff510b996e92919f5
frkatona <apk5101@psu.edu>
2023-12-22 16:01:38 -0500

    added audio and visual effects to clicks

---
+++ Subpages/ChordNetwork/data/json-converter.py
@@ -0,0 +1,42 @@
+import json
+import os
+
+def transform_data(input_file):
+    with open(input_file, 'r') as f:
+        data = json.load(f)
+    nodes = [{"id": item["chord"], "group": item["function"], "notes": item["notes"]} for item in data]
+    links = []
+    for item in data:
+        for type, chords in item["next"][0].items():
+            for chord in chords:
+                links.append({"source": item["chord"], "target": chord, "value": 1, "type": type})
+    output = {"nodes": nodes, "links": links}
+
+    return output
+
+def write_output(output, input_file):
+    output_file = os.path.splitext(input_file)[0] + "-D3.json"
+    with open(output_file, 'w') as f:
+        json.dump(output, f, indent=1)
+
+    # Open the file again in read mode
+    with open(output_file, 'r') as f:
+        file_content = f.read()
+
+    # # Print the file content
+    # print(repr(file_content))
+
+    # Modify the file content
+    file_content = file_content.replace('{\n   ', '{')
+    file_content = file_content.replace(',\n   ', ', ')
+    file_content = file_content.replace('"\n  }', '"}')
+    file_content = file_content.replace('"notes": [\n    ', '"notes": [')
+    file_content = file_content.replace('\n   ]\n  ', ']')
+
+    # Open the file again in write mode and overwrite it with the modified content
+    with open(output_file, 'w') as f:
+        f.write(file_content)
+
+input_file = 'data\chord-flow.json'
+output = transform_data(input_file)
+write_output(output, input_file)
---
+++ Subpages/ChordNetwork/readme.md
@@ -0,0 +1,27 @@
+# TODO
+
+- [x] get chord names to appear as text
+
+- [x] fine tune the force and size so everything is readable and things don't move so far away from each other
+
+- [ ] fancy on-click events
+
+  - [ ] playing the chord (requires reworking the json structure and by extension the json maker script)
+
+  - [ ] bloom that moves along the line to show the various connections and maybe flash their node ids at the top for a moment
+
+- [ ] add connection colors and a legend for them and arrows
+
+- [ ] add an option to change from relative to a given key
+
+- [ ] dark mode
+
+- [ ] distinct click and drag spot for changing the force center
+
+- [ ] polish script that converts the json
+
+- [ ] "create"/"randomize" functionality
+
+  - [ ] create 'x' bars chord progression randomize button with fancy blooms and then plays the progression
+
+  - [ ] allow user to reroll any of the chords in the new progression without affecting the others
